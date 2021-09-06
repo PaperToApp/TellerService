e14bc2b5fdf71fecd56448fac3477f0c3826e7d5
Sandesh Soni <sandeshsoni1@gmail.com>
2021-09-06 21:14:03 +0530

    constants for provided static data

---
+++ lib/watermelon/banking/data_generator.ex
@@ -0,0 +1,14 @@
+defmodule Watermelon.Banking.DummyDataGenerator do
+
+@account_names ["My Checking", "Jimmy Carter", "Ronald Reagan", "George H. W. Bush",  "Bill Clinton", "George W. Bush", "Barack Obama", "Donald Trump"]
+
+@merchants [ "Uber", "Uber Eats", "Lyft", "Five Guys", "In-N-Out Burger", "Chick-Fil-A", "AM", "Apple", "Amazon", "Walmart", "Target", "Hotel Tonight", "Misson Ceviche", "The Caltrain", "Wingstop", "Slim Chickens", "CVS", "Duane Reade", "Walgreens", "McDonald's", "Burger King", "KFC", "Popeye's", "Shake Shack", "Lowe's", "Costco", "Kroger", "iTunes", "Spotify", "Best Buy", "TJ Maxx", "Aldi", "Dollar", "Macy", "H.E. Butt", "Dollar Tree", "Verizon Wireless", "Sprint PCS", "T-Mobi ", "Starbucks", "7-Eleven", "AT&T Wireless", "Rite Aid", "Nordstrom", "Ross", "Gap", "Bed, Bath & Beyond", "J.C. Penney", "Subway", "O'Reilly", "Wendy's", "Dunkin", "Petsmart", "Dick's Sporting Goods", "Sears", "Staples", "Domino's Pizza", "Piz", "Papa John's", "IKEA", "Office Depot", "Foot Locker", "Lids", "GameStop", "Seph", "Panera", "Williams-Sonoma", "Saks Fifth Avenue", "Chipotle Mexican Grill", "Neiman Marcus", "Jack In The Box", "Sonic", "Shell"]
+
+@merchant_categories [ "accommodation", "advertising", "bar", "charity", "clothing", "dining", "education", "entertainment", "fuel", "groceries", "health", "home", "income", "insurance",  "office", "phone", "service", "shopping", "software", "sport", "tax", "transportation", "utilities"]
+
+@institutions ["Chase", "Bank of America", "Wells Fargo", "Citibank", "Capital One"]
+
+  def generate_transaction do
+
+  end
+end
